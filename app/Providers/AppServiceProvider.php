8709b899441a9f54df73755ca5885bf5628bbe52
blindmelon <bryandetera07@gmail.com>
2023-01-12 20:03:42 +0800

    first commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+
+use Filament\Facades\Filament;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Filament::serving(function () {
+            // Using Laravel Mix
+            Filament::registerTheme(
+                mix('css/app.css'),
+            );
+        });
+    }
+}
